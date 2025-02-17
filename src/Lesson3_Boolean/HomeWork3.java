a63669c33fcf25d1000eab131fd82abed957e916
IavDar <elcandess@gmail.com>
2025-02-17 15:17:21 +0100

    HomeWork3

---
+++ src/Lesson3_Boolean/HomeWork3.java
@@ -0,0 +1,30 @@
+package Lesson3_Boolean;
+
+public class HomeWork3 {
+
+    public static void main(String[] args) {
+
+/*        Задача №1
+
+        Необходимо создать две целочисленные переменные (a, b),
+        присвоить произвольные значения переменным на ваш выбор
+        и вывести следующие строки:
+        a == b - если переменные равны
+        a < b - если переменная a меньше b
+        a > b - если переменная b меньше a
+*/
+
+        int a = 50;
+        int b = 15;
+
+        if (a==b) {
+            System.out.println("a == b");
+        } else if (a<b) {
+            System.out.println("a < b");
+        }
+        else {
+            System.out.println("a > b");
+        }
+
+    }
+}

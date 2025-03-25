5cec7995b5eee256be44b81320feae056ee91597
IavDar <elcandess@gmail.com>
2025-03-25 21:38:21 +0100

    Homework16 Task1

---
+++ src/Homework/Lesson16_Standard_Collections/Homework16/Task1.java
@@ -0,0 +1,40 @@
+package Homework.Lesson16_Standard_Collections.Homework16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+
+    /*Задача №1
+
+    Создать лист и добавить в него следующие слова:
+    White.
+            Tan.
+            Yellow.
+            Orange.
+            Red.
+            Pink.
+            Purple.
+            Blue.
+    Затем удалить из этого списка все цвета в которых встречается буква “L”*/
+
+
+    public static void main(String[] args) {
+
+        List<String> colours = new ArrayList<>();
+        colours.add("White");
+        colours.add("Tan");
+        colours.add("Yellow");
+        colours.add("Orange");
+        colours.add("Red");
+        colours.add("Pink");
+        colours.add("Purple");
+        colours.add("Blue");
+
+        System.out.println(colours);
+
+        colours.removeIf(word -> word.toUpperCase().contains("L"));
+
+        System.out.println(colours);
+    }
+}
